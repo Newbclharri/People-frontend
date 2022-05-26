8d107509777c0153f206b5191df798d6a46d051c
contactharrisc2 <contactharrisc2@gmail.com>
2022-05-25 21:22:44 -0500

    adds Route components leading to page components Index and Show

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,15 @@
+import { Routes, Route } from "react-router";
 import { useState, useEffect } from "react";
+import Index from "../pages/Index";
+import Show from "../pages/Show";
 
 export default function Main(props){
     return (
-        <h1>Main</h1>
+        <main>
+            <Routes>
+                <Route exact path="/" element={<Index />} />
+                <Route path="/people/:id" element={<Show />} />
+            </Routes>
+        </main>
     )
 }
