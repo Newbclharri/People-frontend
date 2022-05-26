c9ecdfe3e4f4cfe7f84698dff83c83fcbaf49760
contactharrisc2 <contactharrisc2@gmail.com>
2022-05-25 22:10:46 -0500

    adds function to request data assigned as props

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,12 +5,32 @@ import Show from "../pages/Show";
 
 export default function Main(props){
     const [people, setPeople] = useState(null); //always assign an initial value to state variable
-    const URL = 'http://localhost:3001/people'
+    const URL = 'http://localhost:3001/people';
+
+    //function for requesting api data
+    const getPeople = async ()=> {
+        const data =await fetch(URL).then(res => res.json());
+        setPeople(data);
+    }
+
+    const createPeople = async person => {
+        //ajax
+        await fetch(URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'Application/json'
+            },
+            body: JSON.stringify(person),
+        });
+        getPeople();
+    }
+
+    useEffect(() => {getPeople()}, [])
 
     return (
         <main>
             <Routes>
-                <Route exact path="/" element={<Index />} />
+                <Route exact path="/" element={<Index people={people} />} />
                 <Route path="/people/:id" element={<Show />} />
             </Routes>
         </main>
